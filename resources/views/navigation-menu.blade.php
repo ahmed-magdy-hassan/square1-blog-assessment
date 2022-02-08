61976d5a414cda1b1489df6202095e7546ffe00d
Ahmed Magdy <trmdy@hotmail.com>
2022-02-08 14:12:44 +0200

    [add] sq1 logo

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('welcome') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <img src="{{asset('images/logo/sq1.jpeg')}}" class="block h-9 w-auto">
                     </a>
                 </div>
 
